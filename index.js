30d0d8095a8ec7b31dd916fc3d8b0f038fa4b242
salman <mohammadsalmankhan3191@gmail.com>
2025-09-12 12:08:10 +0530

    this is how we define scheam for our collection

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ async function main() {
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: number,
+});
