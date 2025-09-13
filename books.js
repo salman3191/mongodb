1d239f0e9bee1efb0076df3e14a09c05d2a10e04
salman <mohammadsalmankhan3191@gmail.com>
2025-09-13 12:01:07 +0530

    correcting previos commit that goes wrong on index.js file actual file is books.js {how we use schema validations }

---
+++ books.js
@@ -0,0 +1,39 @@
+// getting-started.js
+const mongoose = require("mongoose");
+
+main()
+  .then(() => {
+    console.log("connection sucessful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
+}
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+});
+const Book = mongoose.model("Book", bookSchema);
+
+let book1 = new Book({
+  title: "Mathematic XII",
+  author: "RD sharma",
+  price: 1200,
+});
+book1
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
